0dc80ec70d18a0dfb1280d91b2f1e31f16f130d9
Amanda <mandinhamlp@gmail.com>
2016-07-21 12:55:46 -0300

    Changed the Search page style to match Dictionary page style

--- app/www/pages/search/search.js
+++ app/www/pages/search/search.js
@@ -1,18 +1,25 @@
-angular.module('diciomane.pages.search',[])
-  .controller('SearchCtrl', ['DictionarySvc', '$state', function(dictionary, $state) {
+angular.module('diciomane.pages.search', [])
+  .controller('SearchCtrl', ['DictionarySvc', '$state', function(dictionary,
+    $state) {
     var ctrl = this;
     ctrl.entries = {};
     ctrl.entry = "";
 
-    ctrl.search= function(){
+    ctrl.search = function() {
       console.log(ctrl.entry);
-      if(ctrl.entry.length >= 3){
-        dictionary.search(ctrl.entry).then(function(entries){
+      if (ctrl.entry.length >= 3) {
+        dictionary.search(ctrl.entry).then(function(entries) {
           ctrl.entries = entries;
           console.log(entries);
         });
-      }else{
+      } else {
         ctrl.entries = {};
       }
-    }
+    };
+
+    ctrl.openEntry = function(word) {
+      $state.go('entry', {
+        id: word.id
+      })
+    };
   }]);
